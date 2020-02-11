362af7e715f820314183f29f7ac4187c4d92f777
João Diego <jdflorencio@gmail.com>
2020-02-10 22:43:21 -0300

    Base do modulo

---
+++ public/modules/produtos/services/clientesService.js
@@ -0,0 +1,12 @@
+angular.module('clientesService', [])
+.factory('Clientes', function($http) {
+    
+    var exampleFactory = {};
+
+    exampleFactory.all = function() {
+        //return $http.get(API.URL/example);
+    };
+
+    return exampleFactory;
+
+});
---
+++ public/modules/subModules/produto/services/clienteService.js
@@ -0,0 +1,12 @@
+angular.module('clienteService', [])
+.factory('Cliente', function($http) {
+    
+    var exampleFactory = {};
+
+    exampleFactory.all = function() {
+        //return $http.get(API.URL/example);
+    };
+
+    return exampleFactory;
+
+});
