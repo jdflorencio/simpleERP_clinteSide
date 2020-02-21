ffd92fe1f81454d07953fb0e69546b3e963384eb
João Diego <jdflorencio@gmail.com>
2020-02-21 10:45:01 -0300

    adicionando as requisição para a API

--- public/modules/subModules/notaFiscal/services/notaFiscalService.js
+++ public/modules/subModules/notaFiscal/services/notaFiscalService.js
@@ -1,10 +1,10 @@
 angular.module('notaFiscalService', [])
-.factory('NotaFiscal', function($http, configURL, $stateParams) {
+.factory('NotaFiscal', function($http, configURL, $stateParams, $state, ngNotify) {
 
     const { baseURL } = configURL
     const host = `${baseURL}/notaFiscal`    
-    const NotaFiscalFactory = {};
-   
+    const NotaFiscalFactory = {}
+    
 	NotaFiscalFactory.consultarNotaFiscal = function() {
 		$http.get(`${host}/${$stateParams.id}`)
 		.then( ( obj ) => {
@@ -14,7 +14,43 @@ angular.module('notaFiscalService', [])
 		.catch((error) => {
 			console.log(error)
 		})
-	}
+    }
+
+    NotaFiscalFactory.atualizar = function() {
+       return  $http.put(`${host}/`, self.notaFiscal)
+        .then((result =>{						
+            $state.go('notaFiscal', {id: result.data.id});
+            let msg = result.data.sucesso ? result.data.msg :data.error.message
+            ngNotify.set(`${msg}`, {
+                type:'info'
+            });
+        }))
+        .catch ((error)=> {
+
+            if (error.data.error) {
+                message = `${error.data.error.message} em (${error.data.error.error[0]})`
+                type = 'warn'
+            }
+            ngNotify.set(`${message}`, {
+                type: type,
+                theme: 'pastel'
+            })
+        })
+    }
+
+    NotaFiscalFactory.salvar =  function() {
+        return $http.post(`${host}`, self.notaFiscal)
+					.then((result) => {
+						$state.go('notaFiscals');
+						let msg = result.data.sucesso ? result.data.msg : result.data.error.message
+						ngNotify.set(`${msg}`, {type:'info',  theme: 'pastel'});
+					}).catch( error => {
+						ngNotify.set(`${message}`, {
+							type: type,
+							theme: 'pastel'
+						})
+				})
+    }
 
     return NotaFiscalFactory;
 
