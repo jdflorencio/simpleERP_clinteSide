230a12c796d120018df6503a0ba36c6a2efdd652
João Diego <jdflorencio@gmail.com>
2020-02-21 10:44:33 -0300

    retirando as requisições para API para ser implementada no factory

--- public/modules/subModules/notaFiscal/controllers/notaFiscalCtrl.js
+++ public/modules/subModules/notaFiscal/controllers/notaFiscalCtrl.js
@@ -36,17 +36,6 @@ angular.module('notaFiscalCtrl', ['notaFiscalService'])
 		console.log('aqui',self.notaFiscal)	
 	}
 
-	// self.consultarNotaFiscal = function() {
-	// 	$http.get(`${host}/${$stateParams.id}`)
-	// 	.then( ( obj ) => {
-	// 		const { result } =  obj.data
-	// 		self.notaFiscal = result
-	// 	})
-	// 	.catch((error) => {
-	// 		console.log(error)
-	// 	})
-	// }
-
 	self.cancelar = function() {
 		$state.go('notaFiscals');
 	}
@@ -56,38 +45,10 @@ angular.module('notaFiscalCtrl', ['notaFiscalService'])
 		let type = 'error'
 		switch ("id" in $stateParams && $stateParams.id != '' ) {
 			case true:
-					const result  = $http.put(`${host}/`, self.notaFiscal)
-					.then((result =>{						
-						$state.go('notaFiscals', {id: result.data.id});
-						let msg = result.data.sucesso ? result.data.msg :data.error.message
-						ngNotify.set(`${msg}`, {
-							type:'info'
-						});
-					}))
-					.catch ((error)=> {
-
-						if (error.data.error) {
-							message = `${error.data.error.message} em (${error.data.error.error[0]})`
-							type = 'warn'
-						}
-						ngNotify.set(`${message}`, {
-							type: type,
-							theme: 'pastel'
-						})
-					})
+					NotaFiscal.atualizar()
 				break
-			case false:
-				$http.post(`${host}`, self.notaFiscal)
-					.then((result) => {
-						$state.go('notaFiscals');
-						let msg = result.data.sucesso ? result.data.msg : result.data.error.message
-						ngNotify.set(`${msg}`, {type:'info',  theme: 'pastel'});
-					}).catch( error => {
-						ngNotify.set(`${message}`, {
-							type: type,
-							theme: 'pastel'
-						})
-				})
+			case false:				
+				NotaFiscal.salvar()
 		}
 	}
 
